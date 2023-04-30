a3143317e9b63a70f82c046683e584bb5615cc96
vova_lovalova <rudchenko.vova@mail.ru>
2023-04-30 19:50:25 +0300

    тесты lint, бэм, валидатор

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,8 @@
-import { iosVhFix } from './utils/ios-vh-fix';
-import { initModals } from './modules/modals/init-modals';
-import { Form } from './modules/form-validate/form';
+import {iosVhFix} from './utils/ios-vh-fix';
+import {initModals} from './modules/modals/init-modals';
+import {Form} from './modules/form-validate/form';
 
-import Swiper, { Navigation } from 'swiper';
+import Swiper, {Navigation} from 'swiper';
 
 Swiper.use([Navigation]);
 
@@ -42,16 +42,18 @@ window.addEventListener('DOMContentLoaded', () => {
   const coachesPrev = document.querySelector('.coaches__swiper-button-prev');
   const coachesNext = document.querySelector('.coaches__swiper-button-next');
 
-  coachesPrev.addEventListener('keydown', (e) => {
-    if (e.keyCode === 13) {
-      swiperCoaches.slidePrev();
-    }
-  });
-  coachesNext.addEventListener('keydown', (e) => {
-    if (e.keyCode === 13) {
-      swiperCoaches.slideNext();
-    }
-  });
+  if (coachesPrev && coachesNext) {
+    coachesPrev.addEventListener('keydown', (e) => {
+      if (e.keyCode === 13) {
+        swiperCoaches.slidePrev();
+      }
+    });
+    coachesNext.addEventListener('keydown', (e) => {
+      if (e.keyCode === 13) {
+        swiperCoaches.slideNext();
+      }
+    });
+  }
 
   // СЛАЙДЕР ОТЗЫВЫ
   let swiperFeedback = new Swiper('.feedback__swiper', {
@@ -67,21 +69,21 @@ window.addEventListener('DOMContentLoaded', () => {
     },
   });
 
-
   const feedbackPrev = document.querySelector('.feedback__swiper-button-prev');
   const feedbackNext = document.querySelector('.feedback__swiper-button-next');
 
-  feedbackPrev.addEventListener('keydown', (e) => {
-    if (e.keyCode === 13) {
-      swiperFeedback.slidePrev();
-    }
-  });
-  feedbackNext.addEventListener('keydown', (e) => {
-    if (e.keyCode === 13) {
-      swiperFeedback.slideNext();
-    }
-  });
-
+  if (feedbackPrev && feedbackNext) {
+    feedbackPrev.addEventListener('keydown', (e) => {
+      if (e.keyCode === 13) {
+        swiperFeedback.slidePrev();
+      }
+    });
+    feedbackNext.addEventListener('keydown', (e) => {
+      if (e.keyCode === 13) {
+        swiperFeedback.slideNext();
+      }
+    });
+  }
 
   // КАРТОЧКИ АБОНЕМЕНТОВ
 
@@ -91,50 +93,52 @@ window.addEventListener('DOMContentLoaded', () => {
   let membershipsPeriodArray = membershipsPeriodList.querySelectorAll('li');
   let membershipsTypeListArray = document.querySelectorAll('.memberships__type-list');
 
-  membershipsPeriodList.style.display = 'flex';
+  if (membershipsPeriodArray && membershipsPeriodList && membershipsTypeListArray) {
+    membershipsPeriodList.style.display = 'flex';
 
-  let showMembershipsTypeList = function () {
-    for (let i = 0; membershipsPeriodArray.length > i; i++) {
+    let showMembershipsTypeList = function () {
+      for (let i = 0; membershipsPeriodArray.length > i; i++) {
 
-      if (membershipsPeriodArray[i].classList.contains('memberships__period-item--active')) {
-        membershipsTypeListArray[i].style.display = 'grid';
-      } else {
-        membershipsTypeListArray[i].style.display = 'none';
-      }
-
-    }
-  };
-
-  showMembershipsTypeList();
-
-  for (let i = 0; membershipsPeriodArray.length > i; i++) {
-
-    let onPeriodClick = function () {
-      for (let j = 0; membershipsPeriodArray.length > j; j++) {
-        if (i === j) {
-          membershipsPeriodArray[j].classList.add('memberships__period-item--active');
+        if (membershipsPeriodArray[i].classList.contains('memberships__period-item--active')) {
+          membershipsTypeListArray[i].style.display = 'grid';
         } else {
-          membershipsPeriodArray[j].classList.remove('memberships__period-item--active');
+          membershipsTypeListArray[i].style.display = 'none';
         }
+
       }
-      showMembershipsTypeList();
     };
 
-    membershipsPeriodArray[i].addEventListener('click', onPeriodClick);
-  }
+    showMembershipsTypeList();
 
-  // тени от цены
+    for (let i = 0; membershipsPeriodArray.length > i; i++) {
 
-  for (let j = 0; membershipsTypeListArray.length > j; j++) {
-    let membershipsCardsArray = membershipsTypeListArray[j].querySelectorAll('li');
+      let onPeriodClick = function () {
+        for (let j = 0; membershipsPeriodArray.length > j; j++) {
+          if (i === j) {
+            membershipsPeriodArray[j].classList.add('memberships__period-item--active');
+          } else {
+            membershipsPeriodArray[j].classList.remove('memberships__period-item--active');
+          }
+        }
+        showMembershipsTypeList();
+      };
 
-    for (let i = 0; membershipsCardsArray.length > i; i++) {
-      let price = membershipsCardsArray[i].querySelector('p');
-      let priceValue = price.textContent;
-      let priceShadow = document.createElement('span');
-      let priceField = membershipsCardsArray[i].querySelector('.memberships-card__price');
-      priceShadow.textContent = priceValue;
-      priceField.appendChild(priceShadow);
+      membershipsPeriodArray[i].addEventListener('click', onPeriodClick);
+    }
+
+    // ТЕНИ ОТ ЦЕН
+
+    for (let j = 0; membershipsTypeListArray.length > j; j++) {
+      let membershipsCardsArray = membershipsTypeListArray[j].querySelectorAll('li');
+
+      for (let i = 0; membershipsCardsArray.length > i; i++) {
+        let price = membershipsCardsArray[i].querySelector('p');
+        let priceValue = price.textContent;
+        let priceShadow = document.createElement('span');
+        let priceField = membershipsCardsArray[i].querySelector('.memberships-card__price');
+        priceShadow.textContent = priceValue;
+        priceField.appendChild(priceShadow);
+      }
     }
   }
 
@@ -144,32 +148,34 @@ window.addEventListener('DOMContentLoaded', () => {
   let playButton = about.querySelector('.about__play-button');
   let video = document.querySelector('#video');
 
-  let insertIframe = function () {
-    video.remove();
-    let iframe = document.createElement('iframe');
-    iframe.classList.add('about__video');
-    iframe.setAttribute('width', 364);
-    iframe.setAttribute('height', 228);
-    iframe.setAttribute('src', 'https://www.youtube-nocookie.com/embed/9TZXsZItgdw?autoplay=1');
-    iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share');
-    iframe.setAttribute('frameborder', 0);
-    about.appendChild(iframe);
-  };
+  if (about && playButton && video) {
+    let insertIframe = function () {
+      video.remove();
+      let iframe = document.createElement('iframe');
+      iframe.classList.add('about__video');
+      iframe.setAttribute('width', 364);
+      iframe.setAttribute('height', 228);
+      iframe.setAttribute('src', 'https://www.youtube-nocookie.com/embed/9TZXsZItgdw?autoplay=1');
+      iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share');
+      iframe.setAttribute('frameborder', 0);
+      about.appendChild(iframe);
+    };
 
-  let onPlayButtonClick = function () {
-    playButton.removeEventListener('click', onPlayButtonClick);
-    insertIframe();
-  };
-
-  let onPlayButtonKeydown = function (e) {
-    if (e.keyCode === 13) {
-      playButton.removeEventListener('keydown', onPlayButtonKeydown);
+    let onPlayButtonClick = function () {
+      playButton.removeEventListener('click', onPlayButtonClick);
       insertIframe();
-    }
-  };
+    };
 
-  playButton.addEventListener('click', onPlayButtonClick);
-  playButton.addEventListener('keydown', onPlayButtonKeydown);
+    let onPlayButtonKeydown = function (e) {
+      if (e.keyCode === 13) {
+        playButton.removeEventListener('keydown', onPlayButtonKeydown);
+        insertIframe();
+      }
+    };
+
+    playButton.addEventListener('click', onPlayButtonClick);
+    playButton.addEventListener('keydown', onPlayButtonKeydown);
+  }
 
   // Utils
   // ---------------------------------
